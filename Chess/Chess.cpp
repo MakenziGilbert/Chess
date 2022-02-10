bd875af13a62c27b48b8682e3ea1b8eb6c4d0abf
Maksym Skorbatiuk <57547446+MakenziGilbert@users.noreply.github.com>
2022-02-10 14:55:12 +0200

    add visually selected figure

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -17,7 +17,8 @@ bool flag = false;
 bool white;
 bool typeMove = false;
 bool findMouse = false;
-const double sizeCell = 57.5;
+double sizeCellx = 57.5;
+double sizeCelly = 57.5;
 ofstream fout;
 ifstream fin;
 
@@ -28,10 +29,8 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    Failed(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -82,7 +81,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
     hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
-    HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,
+    HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 475, 515, nullptr, nullptr, hInstance, nullptr);
 
     hBoard = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP2));
@@ -221,10 +220,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         GetCursorPos(&p2);//получения координат места ходу
         ScreenToClient(hWnd, &p2);
 
-        ps1.x = p1.x / sizeCell;//получение номеру клетки
-        ps2.x = p2.x / sizeCell;
-        ps1.y = p1.y / sizeCell;
-        ps2.y = p2.y / sizeCell;
+        ps1.x = p1.x / sizeCellx;//получение номеру клетки
+        ps2.x = p2.x / sizeCellx;
+        ps1.y = p1.y / sizeCelly;
+        ps2.y = p2.y / sizeCelly;
 
         if (ps1.x != ps2.x || ps1.y != ps2.y)
         {
@@ -237,24 +236,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             {
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_BLACK), hWnd, Failed);
             }
-
-            InvalidateRect(hWnd, NULL, true);
         }
         else
         {
             typeMove = true;
         }
+
+        InvalidateRect(hWnd, NULL, true);
     }
     break;
-    case WM_RBUTTONUP:
+    case  WM_RBUTTONUP:
     {
         if (typeMove)
         {
             GetCursorPos(&p2);//получения координат места ходу
             ScreenToClient(hWnd, &p2);
 
-            ps2.x = p2.x / sizeCell;
-            ps2.y = p2.y / sizeCell;
+            ps2.x = p2.x / sizeCellx;
+            ps2.y = p2.y / sizeCelly;
 
             cb.Move(ps1, ps2);//движение фигуры
             if (cb.win == 1)//проверка н то что игра не законцена
@@ -280,6 +279,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         RECT Rect;
         GetClientRect(hWnd, &Rect);
 
+        sizeCellx = (Rect.right - Rect.left) / 8.;
+        sizeCelly = (Rect.bottom - Rect.top) / 8.;
+
         HDC memDC = CreateCompatibleDC(hdc);
         HBITMAP hBM = CreateCompatibleBitmap(hdc, Rect.right, Rect.bottom);
         SelectObject(memDC, hBM);
@@ -300,10 +302,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         for (int i = 0; i < 8; i++)
             for (int j = 0; j < 8; j++)
             {
-                p.x = i * sizeCell;
-                p.y = j * sizeCell;
+                p.x = i * sizeCellx;
+                p.y = j * sizeCelly;
 
-                COLORREF col = GetPixel(memDC, p.x + 10, p.y + 10);
+                COLORREF col = GetPixel(memDC, p.x + sizeCellx / 2, p.y + sizeCelly / 2);
                if (col != COLORREF(0x000000))
                 {
                     GetObject(hwChess, sizeof(BITMAP), &Bitmapp);
@@ -321,22 +323,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     {
                     case 'K':
 
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 0, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 0, 0, 330, 350, SRCCOPY);
                         break;
                     case 'Q':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 330, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 330, 0, 330, 350, SRCCOPY);
                         break;
                     case 'O':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 670, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 670, 0, 330, 350, SRCCOPY);
                         break;
                     case 'N':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1000, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1000, 0, 330, 350, SRCCOPY);
                         break;
                     case 'T':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1340, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1340, 0, 330, 350, SRCCOPY);
                         break;
                     case 'P':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1670, 0, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1670, 0, 330, 350, SRCCOPY);
                         break;
                     }
                 }
@@ -345,32 +347,47 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     switch (cb.board[j][i][1])
                     {
                     case 'K':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 0, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 0, 350, 330, 350, SRCCOPY);
                         break;
                     case 'Q':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 330, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 330, 350, 330, 350, SRCCOPY);
                         break;
                     case 'O':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 670, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 670, 350, 330, 350, SRCCOPY);
                         break;
                     case 'N':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1000, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1000, 350, 330, 350, SRCCOPY);
                         break;
                     case 'T':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1340, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1340, 350, 330, 350, SRCCOPY);
                         break;
                     case 'P':
-                        StretchBlt(memDC, p.x, p.y, sizeCell, sizeCell, hCompatibleDC, 1670, 350, 330, 350, SRCCOPY);
+                        StretchBlt(memDC, p.x, p.y, sizeCellx, sizeCelly, hCompatibleDC, 1670, 350, 330, 350, SRCCOPY);
                         break;
                     }
                 }
             }
-
+        
+        if (typeMove)
+        {
+            POINT select;
+            SelectObject(memDC, CreatePen(PS_SOLID, 4, RGB(245, 220, 0)));
+            select.x = p1.x / sizeCellx;
+            select.y = p1.y / sizeCelly;
+            select.x *= sizeCellx;
+            select.y *= sizeCelly;
+            
+            MoveToEx(memDC, select.x, select.y, NULL);
+            LineTo(memDC, select.x + sizeCellx, select.y);
+            LineTo(memDC, select.x + sizeCellx, select.y + sizeCelly);
+            LineTo(memDC, select.x, select.y + sizeCelly);
+            LineTo(memDC, select.x, select.y);
+        }
 
         if (findMouse)
         {
-            p.x = p1.x / sizeCell;
-            p.y = p1.y / sizeCell;
+            p.x = p1.x / sizeCellx;
+            p.y = p1.y / sizeCelly;
 
             COLORREF col = GetPixel(memDC, mousePos.x, mousePos.y);
             if (col != COLORREF(0x000000))
@@ -390,22 +407,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 {
                 case 'K':
 
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 0, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 0, 0, 330, 350, SRCCOPY);
                     break;
                 case 'Q':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 330, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 330, 0, 330, 350, SRCCOPY);
                     break;
                 case 'O':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 670, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 670, 0, 330, 350, SRCCOPY);
                     break;
                 case 'N':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1000, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1000, 0, 330, 350, SRCCOPY);
                     break;
                 case 'T':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1340, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1340, 0, 330, 350, SRCCOPY);
                     break;
                 case 'P':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1670, 0, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1670, 0, 330, 350, SRCCOPY);
                     break;
                 }
             }
@@ -414,22 +431,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 switch (cb.board[p.y][p.x][1])
                 {
                 case 'K':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 0, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 0, 350, 330, 350, SRCCOPY);
                     break;
                 case 'Q':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 330, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 330, 350, 330, 350, SRCCOPY);
                     break;
                 case 'O':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 670, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 670, 350, 330, 350, SRCCOPY);
                     break;
                 case 'N':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1000, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1000, 350, 330, 350, SRCCOPY);
                     break;
                 case 'T':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1340, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1340, 350, 330, 350, SRCCOPY);
                     break;
                 case 'P':
-                    StretchBlt(memDC, mousePos.x - sizeCell / 2, mousePos.y - sizeCell / 2, sizeCell, sizeCell, hCompatibleDC, 1670, 350, 330, 350, SRCCOPY);
+                    StretchBlt(memDC, mousePos.x - sizeCellx / 2, mousePos.y - sizeCelly / 2, sizeCellx, sizeCelly, hCompatibleDC, 1670, 350, 330, 350, SRCCOPY);
                     break;
                 }
             }
